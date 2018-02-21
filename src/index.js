7452dd2f37f14a7c7da8e94966d4269a1f905627
Shamber Maxim <36640388+nowifi4u@users.noreply.github.com>
2018-02-22 00:50:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 === 1) return false
+  let configs = bracketsConfig.length
+  
+  let i = 0
+  let cc = 0
+  while (str.length -1 !== i){
+    for (let j = 0; j < configs; j++){
+      if (str.length === 0) return true
+      if (i === -1) i = 0
+      if (cc === configs){
+        i++
+        cc = 0
+      }
+      if (str[i] === bracketsConfig[j][0] && str[i+1] === bracketsConfig[j][1]){
+        str = str.replace(bracketsConfig[j][0]+bracketsConfig[j][1],'')
+        i--
+        cc = 0
+      }
+      else cc++
+    }
+  }
+  if (str.length === 0) return true
+  else return false
 }
